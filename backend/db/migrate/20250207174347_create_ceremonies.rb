ad028237880dd0d3738ee01da1f6942b39230f05
walczyk <k.walczyk@fibcode.com>
2025-02-07 21:51:18 +0100

    add name presence db validation

--- backend/db/migrate/20250207174347_create_ceremonies.rb
+++ backend/db/migrate/20250207174347_create_ceremonies.rb
@@ -3,7 +3,7 @@ class CreateCeremonies < ActiveRecord::Migration[7.2]
     enable_extension "pgcrypto"
 
     create_table :ceremonies, id: :uuid do |t|
-      t.string :name, limit: 255
+      t.string :name, limit: 255, null: false
       t.datetime :event_date
       t.datetime :deleted_at
 
