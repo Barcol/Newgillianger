1b6d4524f3ab6bd93b30f3ae48f3d707d7dd704d
walczyk <k.walczyk@fibcode.com>
2025-02-13 17:56:19 +0100

    add discarded trait to ceremony factory bot

--- backend/spec/factories/ceremonies.rb
+++ backend/spec/factories/ceremonies.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :ceremony do
     name { "Okret nasz wplynie w mgle" }
     event_date { Time.now + 1.week }
+    deleted_at { nil }
+
+    trait :discarded do
+      deleted_at { Time.current }
+    end
   end
 end
