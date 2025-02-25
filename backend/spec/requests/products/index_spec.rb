a534283b2c2d7212b32838ba7b07628bdec4465e
walczyk <k.walczyk@fibcode.com>
2025-02-25 19:31:26 +0100

    test if product index doesnt contain inactive products

--- backend/spec/requests/products/index_spec.rb
+++ backend/spec/requests/products/index_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :request do
   let!(:ceremony) { create(:ceremony) }
+  let!(:product) { create(:product, :inactive, title: "Kurkuma niedobra") }
 
   before do
     create_list(:product, 15, ceremony: ceremony)
+    create_list(:product, 5, :inactive, ceremony: ceremony)
   end
 
   context "when products exist" do
@@ -28,6 +30,21 @@ RSpec.describe ProductsController, type: :request do
 
       expect(json_response['products'].length).to eq(5)
       expect(json_response['meta']['current_page']).to eq(2)
+    end
+
+    it "includes pagination metadata" do
+      get "/products?page=1&per_page=10"
+
+      json_response = JSON.parse(response.body)
+      expect(json_response["meta"]).to include("current_page", "total_pages", "total_count")
+    end
+
+    it "returns only active products" do
+      get "/products?page=1&per_page=100"
+
+      json_response = JSON.parse(response.body)
+      expect(json_response['products'].length).to eq(15)
+      expect(response.body).not_to include("Kurkuma niedobra")
     end
   end
 end
