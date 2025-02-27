0cde5c26064b7e9c11e354f9b764bdb26f1424c6
walczyk <k.walczyk@fibcode.com>
2025-02-27 18:50:06 +0100

    update bundler and bump gems; solve deprecation timezone warnings

--- backend/config/environments/test.rb
+++ backend/config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # To opt in to the behaviour of the reviver to_time will always preserwe the full timezone.
+  config.active_support.to_time_preserves_timezone = :zone
 end
