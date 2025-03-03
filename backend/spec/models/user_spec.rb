d68b40b7fc43a64da72fc895bc3830485e1c3fd8
walczyk <k.walczyk@fibcode.com>
2025-03-03 13:06:44 +0100

    update tests to meet supervisor standards

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,67 +1,88 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject {
-    described_class.new(
-      email: "macoomba@sk.pl",
-      password: "thisissikret"
-    )
-  }
+  let(:email) { "normalguy@example.com" }
+  let(:password) { "password123" }
+
+  subject { build(:user, email: email, password: password) }
 
   it "is valid with valid attributes" do
     expect(subject).to be_valid
   end
 
-  context "when password format is invalid" do
-    it "is not valid without a password" do
-      subject.password = nil
-      expect(subject).not_to be_valid
+  context "when password is invalid" do
+    context "when password is nil" do
+      let(:password) { nil }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
 
-    it "is not valid with too long password" do
-      subject.email = "a" * 256
-      expect(subject).not_to be_valid
+    context "when password is too long" do
+      let(:password) { "a" * 256 }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
 
-    it "is not valid with too stort password" do
-      subject.email = "shorty"
-      expect(subject).not_to be_valid
+    context "when password is too short" do
+      let(:password) { "short" }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
   end
 
   context "when email format is invalid" do
-    it "is not valid without an email" do
-      subject.email = nil
-      expect(subject).not_to be_valid
+    context "when email is nil" do
+      let(:email) { nil }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
 
-    it "is not valid with too long email" do
-      subject.email = "a" * 256
-      expect(subject).not_to be_valid
+    context "when email is too long" do
+      let(:email) { "a" * 244 + "@example.com" }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
 
-    it "is not valid without @ symbol" do
-      subject.email = "exampleexample.com"
-      expect(subject).not_to be_valid
-      expect(subject.errors[:email]).to include("is invalid")
+    context "when email is without @ symbol" do
+      let(:email) { "exampleexample.com" }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
 
-    it "is not valid without domain" do
-      subject.email = "example@"
-      expect(subject).not_to be_valid
-      expect(subject.errors[:email]).to include("is invalid")
+    context "when email is without domain" do
+      let(:email) { "example@" }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
 
-    it "is not valid with spaces" do
-      subject.email = "example @example.com"
-      expect(subject).not_to be_valid
-      expect(subject.errors[:email]).to include("is invalid")
+    context "when email contains spaces" do
+      let(:email) { "example @example.com" }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
 
-    it "is not valid with multiple @ symbols" do
-      subject.email = "example@example@example.com"
-      expect(subject).not_to be_valid
-      expect(subject.errors[:email]).to include("is invalid")
+    context "when email contains multiple @ symbols" do
+      let(:email) { "example@example@example.com" }
+
+      it "is not valid" do
+        expect(subject).not_to be_valid
+      end
     end
   end
 end
