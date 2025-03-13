cf00cc44e7bbac0c28837173919f6cc3b686f524
walczyk <k.walczyk@fibcode.com>
2025-03-13 14:15:57 +0100

    add user authentication

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  private
+
+  def authenticate_user
+    header = request.headers["Authorization"]
+    token = header.split(" ").last if header
+
+    begin
+      secret_key = ENV["CREDENTIALS_SECRET_KEY"]
+      decoded = JWT.decode(token, secret_key)[0]
+      @current_user = User.find(decoded["user_id"])
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user
+  end
 end
