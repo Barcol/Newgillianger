1b6ace28527d32881ac9267c10d02178b0a88312
walczyk <k.walczyk@fibcode.com>
2025-04-05 20:55:08 +0200

    change mailer user instance var to normal one

--- backend/app/mailers/password_mailer.rb
+++ backend/app/mailers/password_mailer.rb
@@ -2,9 +2,9 @@ class PasswordMailer < ApplicationMailer
   default from: "office@newgillianger.com"
 
   def reset_password_email
-    @user = params[:user]
-    @reset_token = @user.reset_password_token
+    user = params[:user]
+    @reset_token = user.reset_password_token
 
-    mail(to: @user.email, subject: "Newgillianger password reset")
+    mail(to: user.email, subject: "Newgillianger password reset")
   end
 end
